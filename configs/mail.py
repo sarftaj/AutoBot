598fda587a95eb490c0ca61fefe40c0187df3fe9
sarftaj <savvythepig@gmail.com>
2023-03-23 09:58:42 -0400

    0.7

--- configs/mail.py
+++ configs/mail.py
@@ -1,4 +1,6 @@
 import smtplib
+from extra import mailpresets
+import subprocess
 
 #info
 print("--------------------------")
@@ -6,10 +8,20 @@ print("--------------------------")
 #app password for Google
 password = "gfnaoaftdbdwmydy"
 
-message1 = input("message to send: ")
+message1 = input("""what message to send
+1: - Entertainment system upgrade script 
+2: - Religion template
+3: - custom template
 
-rec = input("email to spam: ")
 
+     : """)
+
+if message1 == "1":
+    message1 = mailpresets.mailscript1
+if message1 == "2":
+    message1 = mailpresets.mailscript2
+if message1 == "3":
+    message1 = mailpresets.mailscript3
 
 
 
